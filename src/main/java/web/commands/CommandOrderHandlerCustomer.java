232423ec2e481a10634c96e99014eb6084a217b6
Kofoed <Kofoedsan@gmail.com>
2021-05-24 14:57:02 +0200

    nu vises tegning med svg2

--- src/main/java/web/commands/CommandOrderHandlerCustomer.java
+++ src/main/java/web/commands/CommandOrderHandlerCustomer.java
@@ -3,15 +3,18 @@ package web.commands;
 import business.entities.Carport;
 import business.entities.Order;
 import business.entities.materials.BillOfMaterials;
+import business.entities.materials.ByggeMateriale;
 import business.exceptions.UserException;
 import business.services.CarportFacade;
 import business.services.OrderFacade;
 import business.services.UserFacade;
 import business.util.CalculateBOM;
+import business.util.CalculateBOM2;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 public class CommandOrderHandlerCustomer extends CommandProtectedPage {
 
@@ -34,6 +37,9 @@ public class CommandOrderHandlerCustomer extends CommandProtectedPage {
         BillOfMaterials bom;
         carport = carportFacade.getCarportById(carportId);
         bom = CalculateBOM.calculateMaterials(carport);
+        CalculateBOM2 bom2 = new CalculateBOM2(carport);
+        List<ByggeMateriale> stykliste = bom2.calculateMaterials();
+        session.setAttribute("stykliste", stykliste);
         session.setAttribute("bom", bom);
         session.setAttribute("carport", carport);
         return showSVGCommandcustomer.execute(request, response);
--- src/main/java/web/commands/ShowSVGCommandcustomer.java
+++ src/main/java/web/commands/ShowSVGCommandcustomer.java
@@ -2,13 +2,16 @@ package web.commands;
 
 import business.entities.Carport;
 import business.entities.materials.BillOfMaterials;
+import business.entities.materials.Materiale;
 import business.exceptions.UserException;
 import business.services.SVG;
 import business.util.CalculateElementsSVG;
+import business.util.CalculateElementsSVG2;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 public class ShowSVGCommandcustomer extends CommandProtectedPage {
     public ShowSVGCommandcustomer(String pageToShow, String role) {
@@ -28,7 +31,6 @@ public class ShowSVGCommandcustomer extends CommandProtectedPage {
         bom = (BillOfMaterials) session.getAttribute("bom");
         carport = (Carport) session.getAttribute("carport");
 
-
         String viewBoxTemplate = "0 0 %s %s";
         String xCanvas = String.valueOf(carport.getLength()+100);
         String yCanvas = String.valueOf(carport.getWidth()+100);
@@ -36,15 +38,19 @@ public class ShowSVGCommandcustomer extends CommandProtectedPage {
 
         svg = new SVG(0, 0, viewBox, 100, 100);
         CalculateElementsSVG.calculateElements(bom, carport, svg);
-//        svg.closeSvg();
-
-
-
-
-//        stringBuilder.append(svg);
-//        stringBuilder.append(
-
         request.setAttribute("svg", svg.toString());
+
+//        forsøg med ny stykliste
+        List<Materiale> stykliste = (List<Materiale>) session.getAttribute("stykliste");
+        SVG svg2 = new SVG(0, 0, viewBox, 100, 100);
+        CalculateElementsSVG2.calculateElements(stykliste, carport, svg2);
+        request.setAttribute("svg2", svg2.toString());
+
+
+
+
+
+
 
         return pageToShow;
 
